812a34c0a356d659d5d3569c330b40eed8b8b288
Deb Guin <sukumarguin10@gmail.com>
2026-07-04 12:27:33 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_VERSION = 'ft-v14';
+const CACHE_VERSION = 'ft-v15';
 const SHELL_CACHE   = `${CACHE_VERSION}-shell`;
 const STATIC_CACHE  = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
